e1aa4883663e04f56d3135f8e964650289c16985
TaizunJ <jafri.taizun.s@gmail.com>
2025-03-09 23:25:18 -0700

    functions.py : Update Comments for better usability

--- CODE/PY_FILES/functions.py
+++ CODE/PY_FILES/functions.py
@@ -28,7 +28,6 @@ the section for .bench files does not have documentation as of now
 
 
 from tabulate import tabulate
-import contextlib
 
 
 # Define data structure to store details of each node in bench
@@ -104,7 +103,7 @@ def set_nodes(CIRCUIT_BENCH_FILE):
 
 
 # Function to print all nodes in required format :
-def print_output_info(file=None):
+def print_output_info():
     # Count the different types and number of gates
     gate_counts = {}
     for node in nodes.values():
@@ -121,34 +120,32 @@ def print_output_info(file=None):
     for gate_type, count in gate_counts.items():
         table_data.append([f"{gate_type} gates", count])
 
-    # Redirect output to file if provided
-    with contextlib.redirect_stdout(file) if file else contextlib.nullcontext():
-        # Print the table
-        print(tabulate(table_data, headers=["Type", "Count"], tablefmt="grid"))
+    # Print the table
+    print(tabulate(table_data, headers=["Type", "Count"], tablefmt="grid"))
 
-        # Print Fanout :
-        # Example Output: e.g -- NAND-16 : NAND-22, NAND-19
-        print("\n")
-        print('-' * 70)
-        print("\t \t FAN-OUT")
-        print('-' * 70)
-        for node in nodes.values():
-            if node.gate_type != "INPUT" and node.outputs:  # Exclude primary inputs from fan-out section
-                output_names = [f"{out_node.gate_type}-{out_node.name}" for out_node in node.outputs]
-                print(f"{node.gate_type}-{node.name}: {', '.join(output_names)}")
-            elif node.gate_type != "INPUT":
-                print(f"{node.gate_type}-{node.name}: OUTPUT-{node.name}")
+    # Print Fanout :
+    # Example Output: e.g -- NAND-16 : NAND-22, NAND-19
+    print("\n")
+    print('-' * 70)
+    print("\t \t FAN-OUT")
+    print('-' * 70)
+    for node in nodes.values():
+        if node.gate_type != "INPUT" and node.outputs:  # Exclude primary inputs from fan-out section
+            output_names = [f"{out_node.gate_type}-{out_node.name}" for out_node in node.outputs]
+            print(f"{node.gate_type}-{node.name}: {', '.join(output_names)}")
+        elif node.gate_type != "INPUT":
+            print(f"{node.gate_type}-{node.name}: OUTPUT-{node.name}")
 
-        # print("\n")
-        print('-' * 70)
-        print("\t \t FAN-IN")
-        print('-' * 70)
-        for node in nodes.values():
-            if node.gate_type != "INPUT" and node.inputs:
-                input_names = [f"INPUT-{inp_node.name}" if inp_node.gate_type == "-" else f"{inp_node.gate_type}-{inp_node.name}" for inp_node in node.inputs]
-                print(f"{node.gate_type}-{node.name} : {', '.join(input_names)}")
-            elif node.gate_type != "INPUT":
-                print(f"{node.gate_type}-{node.name} : INPUT-{node.name}")
+    # print("\n")
+    print('-' * 70)
+    print("\t \t FAN-IN")
+    print('-' * 70)
+    for node in nodes.values():
+        if node.gate_type != "INPUT" and node.inputs:
+            input_names = [f"INPUT-{inp_node.name}" if inp_node.gate_type == "-" else f"{inp_node.gate_type}-{inp_node.name}" for inp_node in node.inputs]
+            print(f"{node.gate_type}-{node.name} : {', '.join(input_names)}")
+        elif node.gate_type != "INPUT":
+            print(f"{node.gate_type}-{node.name} : INPUT-{node.name}")
 
 
 ####################################################################################
@@ -163,39 +160,30 @@ This section contains functions that are used to parse given .lib(NLDM) file int
     print data about corresponding `slew` and `delay` values and give the following sample output :
 
 Parsing file using the --delays option should give the following output for all cells in the file:
-
->>> python3.7 parser.py --slews --read_nldm sample_NLDM.lib 
-
-    Cell Name          : NAND2_X1
-    Cell Capacitance   : 1.599032
-    Input Slew         : [0.00117378 0.00472397 0.0171859  0.0409838  0.0780596  0.130081
-    0.198535  ]
-    Load Capacitance   : [ 0.365616  1.8549    3.70979   7.41959  14.8392   29.6783   59.3567  ]
-    Cell Delay Table    :
-        0.0074307,0.0112099,0.0157672,0.0247561,0.0426101,0.0782368,0.149445
-        0.00896317,0.0127084,0.0173,0.0263569,0.0442815,0.0799642,0.151206
-        0.0141826,0.0189535,0.0236392,0.0325101,0.0503637,0.0860462,0.157306
-        0.0198673,0.0266711,0.0336357,0.044885,0.0628232,0.098154,0.16922
-        0.0262799,0.0348883,0.043833,0.0586475,0.0818511,0.117889,0.188351
-        0.0334985,0.0438815,0.0546771,0.0727012,0.101569,0.145562,0.216015
-        0.0415987,0.0537162,0.0663517,0.0874425,0.121509,0.174517,0.253405
+>>> cell: NAND2_X1
+        input slews: 0.00117378,0.00472397,0.0171859,0.0409838,0.0780596,0.130081,0.198535
+        load cap: 0.365616,1.854900,3.709790,7.419590,14.839200,29.678300,59.356700
+        delays:
+            0.00743070,0.0112099,0.0157672,0.0247561,0.0426101,0.0782368,0.149445;
+            0.00896317,0.0127084,0.0173000,0.0263569,0.0442815,0.0799642,0.151206;
+            0.0141826,0.0189535,0.0236392,0.0325101,0.0503637,0.0860462,0.157306;
+            0.0198673,0.0266711,0.0336357,0.0448850,0.0628232,0.0981540,0.169220;
+            0.0262799,0.0348883,0.0438330,0.0586475,0.0818511,0.117889,0.188351;
+            0.0334985,0.0438815,0.0546771,0.0727012,0.101569,0.145562,0.216015;
+            0.0415987,0.0537162,0.0663517,0.0874425,0.121509,0.174517,0.253405;
 
 Parsing file using the --slew option should produce the following output for all cells in the file:
-
->>> python3.7 parser.py --slews --read_nldm sample_NLDM.lib 
-
-    Cell Name          : NAND2_X1
-    Cell Capacitance   : 1.599032
-    Input Slew         : [0.00117378 0.00472397 0.0171859  0.0409838  0.0780596  0.130081 0.198535  ]
-    Load Capacitance   : [ 0.365616  1.8549    3.70979   7.41959  14.8392   29.6783   59.3567  ]
-    Output Slew Table  :
-        0.00474878,0.00814768,0.0123804,0.020848,0.0377848,0.0716838,0.139435
-        0.00475427,0.00814708,0.0123814,0.0208446,0.0377762,0.0716641,0.139428
-        0.0077976,0.009978,0.0130179,0.02085,0.0378031,0.0716776,0.13943
-        0.0122628,0.0156758,0.0191464,0.0247382,0.0382858,0.0716833,0.139437
-        0.0178385,0.0220827,0.0266676,0.0342116,0.0458454,0.0726908,0.139429
-        0.0249336,0.0298045,0.0352101,0.0445099,0.0592803,0.0822832,0.139806
-        0.0337631,0.03916,0.0452534,0.0559346,0.0736025,0.100571,0.148264 
+>>> cell: NOR2_X1
+        input slews: 0.00117378,0.00472397,0.0171859,0.0409838,0.0780596,0.130081,0.198535
+        load cap: 0.365616,1.854900,3.709790,7.419590,14.839200,29.678300,59.356700
+        delays:
+            0.0136008,0.0161505,0.0205760,0.0292340,0.0462805,0.0801242,0.147594;
+            0.0142633,0.0167540,0.0211519,0.0298650,0.0470595,0.0810783,0.148696;
+            0.0202027,0.0226399,0.0267281,0.0350583,0.0519006,0.0857406,0.153365;
+            0.0282210,0.0316593,0.0372260,0.0469299,0.0634650,0.0965651,0.163573;
+            0.0381015,0.0422278,0.0490020,0.0610840,0.0815179,0.115113,0.181020;
+            0.0503320,0.0550648,0.0628653,0.0768972,0.101146,0.141009,0.207020;
+            0.0651817,0.0704989,0.0792584,0.0950330,0.122530,0.168656,0.242490;
 
 """
 
@@ -227,7 +215,7 @@ def print_nldm_output_slew_table(NLDM_FILE:str):
         for cell_name, cell_content in cell_blocks:
 
             # Extract cell capacitance
-            cell_capacitance = re.search(r'capacitance\s*:\s*(\d+\.\d+);', cell_content).group(1)
+            cell_capacitance = re.search(r'capacitance\s*:\s*(\d+\.\d+);', cell_content)
 
             # Extract index-1 values from output_slew subsection of .lib file
             index_1 = re.search(r'output_slew\s*\(\w+\)\s*{\s*index_1\s*\("(.*?)"\)', cell_content)
@@ -249,16 +237,6 @@ def print_nldm_output_slew_table(NLDM_FILE:str):
             lut.ost_input_slews = index_1_values         # Index-1 values in output_slew() sub-section of a cell
             lut.ost_load_capacitance = index_2_values    # Index-2 values in output_slew() sub-section of a cell
             lut.output_slew_table = output_slew_table    # Output slew table as a 2D numpy array
-
-            print('\n')
-            print(f'Cell Name          : {lut.cell_name}')
-            print(f'Cell Capacitance   : {lut.capacitance}')
-            print(f'Input Slew         : {lut.ost_input_slews}')
-            print(f'Load Capacitance   : {lut.ost_load_capacitance}')
-            print("Output Slew Table  :")
-            for row in lut.output_slew_table:
-                print(f"    {','.join(map(str, row))}")
-
             
 
 def print_nldm_cell_delay_table(NLDM_FILE:str) :
@@ -272,7 +250,7 @@ def print_nldm_cell_delay_table(NLDM_FILE:str) :
         for cell_name, cell_content in cell_blocks:
 
             # Extract cell capacitance
-            cell_capacitance = re.search(r'capacitance\s*:\s*(\d+\.\d+);', cell_content).group(1)
+            cell_capacitance = re.search(r'capacitance\s*:\s*(\d+\.\d+);', cell_content)
             
             # Extract index-1 values from cell_delay subsection of .lib file
             index_1 = re.search(r'cell_delay\s*\(\w+\)\s*{\s*index_1\s*\("(.*?)"\)', cell_content)
@@ -286,7 +264,8 @@ def print_nldm_cell_delay_table(NLDM_FILE:str) :
             _cell_delay = re.search(r'values\s*\((.*?)\);', cell_content, re.DOTALL).group(1).replace('", \\\n', '').replace('\t', '').replace('"', '')
             rows = _cell_delay.split(' ')
             cell_delay_table = np.array([list(map(float, row.split(','))) for row in rows if row.strip() != ''])
-           
+            print(f'Cell : {cell_name}, Cell Delay Table : {cell_delay_table}')
+
             # Create LUT object for each cell and assign corresponding values :
             lut = LUT()
             lut.cell_name = cell_name
@@ -294,12 +273,3 @@ def print_nldm_cell_delay_table(NLDM_FILE:str) :
             lut.input_slews = index_1_values         # Index-1 values in cell_delay() sub-section of a cell
             lut.load_capacitance = index_2_values    # Index-2 values in cell_delay() sub-section of a cell
             lut.delay_table = cell_delay_table       # Cell delay table as a 2D numpy array
-
-            print('\n')
-            print(f'Cell Name          : {lut.cell_name}')
-            print(f'Cell Capacitance   : {lut.capacitance}')
-            print(f'Input Slew         : {lut.input_slews}')
-            print(f'Load Capacitance   : {lut.load_capacitance}')
-            print("Cell Delay Table    :")
-            for row in lut.delay_table:
-                print(f"    {','.join(map(str, row))}")
